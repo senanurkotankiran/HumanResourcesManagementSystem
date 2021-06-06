b566cf53a44a71b19afc267563bb20202437357e
senanurkotankiran <77417875+senanurkotankiran@users.noreply.github.com>
2021-06-06 15:08:42 +0300

    Human Resources Management System

--- hrms/src/main/java/kodlamaio/hrms/core/adapters/FakeMernisService.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.hrms.core.adapters;
-
-import kodlamaio.hrms.entities.concretes.Candidates;
-
-
-public interface FakeMernisService {
-
-	public boolean chekIfRealPerson(Candidates candidates);
-}
